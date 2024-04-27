f02372fa7e971a279f78ecd8322aec2e11ffd41e
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2024-04-28 00:32:01 +0300

    TgProcess

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ClientSeeder::class,
+            //ClientSeeder::class,
             NavigationSeeder::class,
             PageSeeder::class,
             MoonShineSeeder::class,
