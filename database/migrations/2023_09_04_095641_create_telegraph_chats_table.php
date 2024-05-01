4d64ca64b9285cb5b8537115bc1a9ff6f55cebb5
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2024-05-01 18:28:32 +0300

    TgProcess

--- database/migrations/2023_09_04_095641_create_telegraph_chats_table.php
+++ database/migrations/2023_09_04_095641_create_telegraph_chats_table.php
@@ -14,9 +14,9 @@ return new class () extends Migration {
             $table->string('name')->nullable();
 
             $table->foreignId('telegraph_bot_id')->constrained('telegraph_bots')->cascadeOnDelete();
-            $table->foreignIdFor(Client::class)
-                    ->constrained()
-                    ->nullable();
+            $table->foreignId('client_id')
+                    ->nullable()
+                    ->constrained();
             $table->timestamps();
 
             $table->unique(['chat_id', 'telegraph_bot_id']);
