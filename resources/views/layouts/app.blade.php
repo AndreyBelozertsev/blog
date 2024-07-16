a4e43b1ccbfdab2288f0b4568aac72207f7c6ca3
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2024-07-16 17:02:20 +0300

    fetch:AddYaCounter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,23 @@
 	<link rel="mask-icon" href="./images/safari-pinned-tab.svg" color="#14252C">
 	<meta name="msapplication-TileColor" content="##14252C">
 	<meta name="theme-color" content="#14252C">
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+	(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+	m[i].l=1*new Date();
+	for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+	k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+	ym(97847070, "init", {
+			clickmap:true,
+			trackLinks:true,
+			accurateTrackBounce:true,
+			webvisor:true
+	});
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/97847070" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
 
     @vite(['resources/css/app.css', 'resources/css/scss/main.scss'])
 	
